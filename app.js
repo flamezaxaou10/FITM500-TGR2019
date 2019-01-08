fbe475ee5806e2056999e6b835b795951aca2d70
flamezaxaou10 <flamezaxaou1@gmail.com>
2019-01-08 11:50:19 +0700

    update listUser and addUser

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 8080
+var fs = require("fs")
 
 app.use(bodyParser.json({ limit: '50mb', extended: true }))
 
@@ -12,4 +13,21 @@ app.get('/', (req, res) => {
 var server = app.listen(port, () => {
   var port = server.address().port
   console.log('Start server at port ' + port + ' >> localhost:' + port)
+})
+
+app.get('/listUsers', function (req, res) {
+  fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+     console.log( data )
+     res.end( data )
+  })
+})
+
+app.post('/addUser', function (req, res) {
+  // First read existing users.
+  fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+     data = JSON.parse( data )
+     let key = Date.now()
+     data[key] = req.body
+     res.end( JSON.stringify(data));
+  })
 })

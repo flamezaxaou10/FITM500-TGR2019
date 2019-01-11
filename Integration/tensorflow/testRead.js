2cbd25d80b5e37a015f14c7be8e41ceb06402998
dome1260 <dome.ee1260@gmail.com>
2019-01-11 08:29:13 +0700

    create dataflow module

---
+++ Integration/tensorflow/testRead.js
@@ -0,0 +1,12 @@
+var read = require('./read.js');
+let Tensorflow = require('./tensorflow_provider')
+
+async function main () {
+  let data = await read()
+  console.log('max --->', data.max)
+  const tensorFlow = new Tensorflow(data, 12)
+  await tensorFlow.trainDataScaling()
+  // await tensorFlow.predictDataScaling([[0,0,0,0,0,0.0416435314,65/1801,59/1801]])
+}
+
+main();
